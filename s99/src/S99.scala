12aae6419fd52cd847fdbc6c1899f22bcf1dbaf9
abalone <abalone.bdh@gmail.com>
2014-11-02 07:55:47 +0800

    problem 3 to 6

--- s99/src/S99.scala
+++ s99/src/S99.scala
@@ -6,7 +6,7 @@ import scala.collection.immutable.List
 object S99 extends App{
 
   val list = List(1, 1, 2, 3, 5, 8)
-  // (1) Find the last element of a list
+  // (1) Find the last element of a list.
   def last = (l : List[Any]) => l.last
   println(last(list))
 
@@ -14,5 +14,19 @@ object S99 extends App{
   def penutimate = (l : List[Any]) => l.dropRight(1).last
   println(penutimate(list))
 
+  // (3) Find the Kth element of a list.
+  def nth = (n: Int, l : List[Any]) => l.take(n+1).last
+  println(nth(2, list))
 
+  // (4) Find the number of elements of a list.
+  def length = (l : List[Any]) => l.length
+  println(length(list))
+
+  // (5) Reverse a list.
+  def reverse = (l : List[Any]) => l.reverse
+  println(reverse(list))
+
+  // (6) Find out whether a list is a palindrome.
+  def isPalindrome = (l : List[Any]) => l.take(l.length/2) .equals(l.takeRight(l.length/2).reverse)
+  println(isPalindrome(List(1, 2, 3, 2, 1)))
 }
