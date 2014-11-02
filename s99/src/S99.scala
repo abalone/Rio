7557467404f7dd0aa0e57f277f4aecff74756b18
abalone <abalone.bdh@gmail.com>
2014-11-02 12:16:14 +0800

    problem 8

--- s99/src/S99.scala
+++ s99/src/S99.scala
@@ -31,10 +31,20 @@ object S99 extends App{
   println(isPalindrome(List(1, 2, 3, 2, 1)))
 
   // (7) Flatten a nested list structure.
-  def flatten(l:List[_]) : List[_] = l match {
+  def flatten(l : List[_]) : List[_] = l match {
     case (head : List[_]) :: tail => flatten(head) ::: flatten(tail)
     case head :: tail => head :: flatten(tail)
     case Nil => Nil
   }
   println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
+
+  // (8) Eliminate consecutive duplicates of list elements.
+  def compress[T](l : List[T]) = l.tail.foldLeft(List[T](l.head))((ll:List[T], e) => ll.reverse match{
+    case Nil => ll ::: List[T](e)
+    case lll => if(lll.head.equals(e)) ll else ll ::: List[T](e)
+  })
+  println(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+
+  // (9) Pack consecutive duplicates of list elements into sublists.
+  //println(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
 }
